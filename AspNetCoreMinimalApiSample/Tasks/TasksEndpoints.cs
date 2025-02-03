f94e2363c8e07a0a645c0027e30a333d8aadeded
LAPTOP-4BM3QH1A\koki <kosawa0627@gmail.com>
2025-02-04 01:12:55 +0900

    chore: rename project

---
+++ AspNetCoreMinimalApiSample/Tasks/TasksEndpoints.cs
@@ -0,0 +1,21 @@
+﻿using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+
+namespace aspnetcore_minimal_api_sample.Tasks
+{
+    public static class TasksEndpoints
+    {
+        public static void REgisterTasksEndpoints(this WebApplication app)
+        {
+            app.MapGet("/tasks", async (TaskDb db) =>
+                await db.Tasks.ToListAsync());
+
+            app.MapGet("/tasks/{id}", async (int id, TaskDb db) =>
+            await db.Tasks.FindAsync(id)
+                is Task task
+                    ? Results.Ok(task)
+                    : Results.NotFound());
+
+        }
+    }
+}
